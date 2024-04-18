edfefb537cecb3c832054834a129239abc70fe42
maitha1-nule <nuleahmed@gmail.com>
2024-04-18 22:41:47 +0300

    This is are from the previuous days

---
+++ Wesnet/index.js
@@ -0,0 +1,9 @@
+var slideIndex = 0;
+
+function showSlides(){
+    var slides = document.querySelectorAll('.slide-show-container image');
+    slides[slideIndex].classList.remove('active');
+    slideIndex = (slideIndex + 1) % slides.length;
+    slides[slideIndex].classList.add('acvtive');
+    setTimeout(showSlides, 2000);
+}
