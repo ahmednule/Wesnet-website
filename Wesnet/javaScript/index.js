2f97cdeb487251e3f4aa5eb50f4856c95aebaaf8
maitha1-nule <nuleahmed@gmail.com>
2024-04-21 12:34:38 +0300

    Added js code to the button so that when a button is clicked the contents of the menu come else, they are hidden by default

--- Wesnet/javaScript/index.js
+++ Wesnet/javaScript/index.js
@@ -1,10 +1,14 @@
-/* var slideIndex = 0;
-
-function showSlides(){
-    var slides = document.querySelectorAll('.slide-show-container image');
-    slides[slideIndex].classList.remove('active');
-    slideIndex = (slideIndex + 1) % slides.length;
-    slides[slideIndex].classList.add('acvtive');
-    setTimeout(showSlides, 2000);
-}
-*/
+document.addEventListener("DOMContentLoaded", function() {
+    const toggleButton = document.getElementById("bars");
+    const hiddenList = document.getElementById("hidden-menu");
+    
+    function toggleList() {
+        if (hiddenList.style.display === "none") {
+            hiddenList.style.display = "block";
+        } else {
+            hiddenList.style.display = "none";
+        }
+    }
+    
+    toggleButton.addEventListener("click", toggleList);
+});
